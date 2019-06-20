051c1df3f5f279534e367aa169d6f1d9b2f0a9ba
MH-Zarkhani <m.hassan.zarkhani@gmail.com>
2019-06-20 15:55:29 +0430

    modify users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name',100);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile',13)->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar',100)->nullable();
+            $table->string('website')->nullable();
+            $table->string('verify_code',6)->nullable();
+            $table->timestamp('verified_at')->nullable();
             $table->timestamps();
         });
     }
