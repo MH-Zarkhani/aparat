3479d71cda8880e23e8447443b08544f84cfa684
MH-Zarkhani <m.hassan.zarkhani@gmail.com>
2019-06-20 21:28:56 +0430

    create video_views migration and table

--- database/migrations/2019_06_20_165503_create_video_views_table.php
+++ database/migrations/2019_06_20_165503_create_video_views_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVideosTable extends Migration
+class CreateVideoViewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateVideosTable extends Migration
      */
     public function up()
     {
-        Schema::create('videos', function (Blueprint $table) {
+        Schema::create('video_views', function (Blueprint $table) {
             $table->bigIncrements('id');
-
+           
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')
                 ->references('id')
@@ -23,19 +23,13 @@ class CreateVideosTable extends Migration
                 ->onDelete('set null')
                 ->onUpdate('cascade');
 
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')
+            $table->unsignedBigInteger('video_id');
+            $table->foreign('video_id')
                 ->references('id')
-                ->on('categories')
+                ->on('videos')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
-
-            $table->string('slug', 50);
-            $table->string('title');
-            $table->text('info')->nullable();
-            $table->integer('duration');
-            $table->string('banner')->nullable();
-            $table->timestamp('publish_at')->nullable();
+            
             $table->timestamps();
         });
     }
@@ -47,6 +41,6 @@ class CreateVideosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('videos');
+        Schema::dropIfExists('video_views');
     }
 }
